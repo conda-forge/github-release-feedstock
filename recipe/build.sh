e385ecdd93c103d11501e2c4a041d41b74c2277f
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 01:53:35 -0800

    multiple updates

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go mod init github-release
-go mod vendor -e
-go mod tidy -e
-go build -modcacherw -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files
