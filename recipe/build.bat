2f8703252782d85f9b3d8702a8a12f155d99ad54
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 02:01:00 -0800

    fix build

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,9 +1,6 @@
 @echo on
 
-go mod init github-release || goto :error
-go mod vendor -e || goto :error
-go mod tidy -e || goto :error
-go build  -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w" || goto :error
+go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe -ldflags="-s -w" || goto :error
 go-licenses save . --save_path=license-files || goto :error
 
 goto :eof
